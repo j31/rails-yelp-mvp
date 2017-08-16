33eba1425f24b144797d55b6eca8b0e3cda86a3c
John Fish <j31@users.noreply.github.com>
2017-08-16 12:09:33 +0200

    works to spec but needs formatting

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,55 @@
+class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+    @restaurant.save
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @restaurant
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurant_attributes = Restaurant.create([
+  { name: "McBistro",
+    address: "Paris",
+    phone_number: "000-123-1234",
+    category: "french",
+  },
+  { name: "McAsia",
+    address: "Chinatown",
+    phone_number: "000-123-1235",
+    category: "chinese",
+  },
+  { name: "McItalian",
+    address: "Milan",
+    phone_number: "000-123-1236",
+    category: "italian",
+  },
+  { name: "McSushi",
+    address: "Tokyo",
+    phone_number: "000-123-1237",
+    category: "japanese",
+  }
+]);
